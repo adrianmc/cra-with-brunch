648c2daf4df8b7636dbf7a95d2804ffb195cf5c6
Adrian Li <li.adrianmc@gmail.com>
2016-08-04 21:38:57 +0800

    add all CRA styles

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import style from './App.css';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div id="content">
-        <h1>&nbsp;</h1>
-        <h2>Welcome!</h2>
-        <ul>
-          <li><a href="http://brunch.io">Brunch homepage</a></li>
-          <li><a href="https://facebook.github.io/react/">React.js homepage</a></li>
-        </ul>
+      <div className={style.app}>
+        <div className={style.appHeader}>
+          <img src="logo.svg" className={style.appLogo} alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className={style.appIntro}>
+          To get started, edit <code>app/src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
